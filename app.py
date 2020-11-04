7488f5324ba09f2baa47cc9919385f52d702a0c5
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-11-03 23:16:50 -0300

    Trigger build

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def hello():
     count = redis.incr('hits')
     return 'v3/Hello World! I have been seen {} times.\n'.format(count)
 
+@app.route('/healthcheck')
+def hello():
+    return 'OK'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
